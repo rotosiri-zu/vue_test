53c26d72a13431e617f0a5233533efbf632053c2
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2021-03-23 21:47:14 +0900

    vue.jsコースセクション8: サンプル6 SPA + GoogleBookAPI 107. App.vueからlocalStorageへ保存

--- section8/bookapp/src/App.vue
+++ section8/bookapp/src/App.vue
@@ -2,7 +2,10 @@
   <v-app>
   <Header/>
     <v-main>
-      <router-view/>
+      <v-container>
+        <router-view
+        @add-book-list="addBook"/>
+      </v-container>
     </v-main>
   <Footer/>
   </v-app>
@@ -11,6 +14,7 @@
 <script>
 import Header from '@/global/Header';
 import Footer from '@/global/Footer';
+const STORAGE_KEY = 'books'
 
 export default {
   name: 'App',
@@ -20,8 +24,43 @@ export default {
     Footer
   },
 
-  data: () => ({
-    //
-  }),
+  data(){
+    return {
+      books: [],
+      newBook: null
+    }
+  },
+   mounted() {
+    if (localStorage.getItem(STORAGE_KEY)) {
+      try {
+        this.books = JSON.parse(localStorage.getItem(STORAGE_KEY));
+      } catch(e) {
+        localStorage.removeItem(STORAGE_KEY);
+      }
+    }
+  },
+  methods: {
+    addBook(e) {
+
+      this.books.push({
+        id: this.books.length,
+        title: e.title,
+        image: e.image,
+        description: e.description,
+        readDate: '',
+        memo: ''
+      });
+      // this.newBook = '';
+      this.saveBooks();
+    },
+    removeBook(x) {
+      this.books.splice(x, 1);
+      this.saveBooks();
+    },
+    saveBooks() {
+      const parsed = JSON.stringify(this.books);
+      localStorage.setItem(STORAGE_KEY, parsed);
+    }
+  }
 };
 </script>
