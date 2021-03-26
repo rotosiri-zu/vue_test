c14c9b361a511124eaa971fc04566329d11a8ad2
rotosiri-zu <55967256+rotosiri-zu@users.noreply.github.com>
2021-03-26 20:43:36 +0900

    vue.jsコースセクション8: サンプル6 SPA + GoogleBookAPI 114. LocalStorage一括削除

--- section8/bookapp/src/App.vue
+++ section8/bookapp/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <v-app>
-  <Header/>
+  <Header
+  @delete-local-storage="deleteLocalStorage"
+  />
     <v-main>
       <v-container>
         <router-view
@@ -81,6 +83,15 @@ export default {
     },
     goToEditPage(id){
       this.$router.push(`/edit/${id}`)
+    },
+    deleteLocalStorage(){
+      const isDeleted = 'LocalStorageのテータを削除してもいいですか？'
+      if(window.confirm(isDeleted)){
+        localStorage.setItem(STORAGE_KEY, '');
+        localStorage.removeItem(STORAGE_KEY)
+        this.books = []
+        window.location.reload()
+      }
     }
   }
 };
--- section8/bookapp/src/global/Header.vue
+++ section8/bookapp/src/global/Header.vue
@@ -6,13 +6,22 @@
       dark
     >
       <v-spacer></v-spacer>
+      <v-btn color="error"
+      @click="deleteLocalStorage">
+      削除する
+      </v-btn>
     </v-app-bar>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'Header',
+  methods:{
+    deleteLocalStorage(){
+      this.$emit('delete-local-storage')
+    }
+  }
 }
 </script>
 
